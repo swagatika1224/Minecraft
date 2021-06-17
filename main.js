9e133f2c0e9d29e4234965773fd2364de4ce9734
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-06-16 18:51:05 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ canvas.add(player_object);
 
 
 
-function player_update() {
+function new_image(get_image) {
     fabric.Image.fromURL(get_image, function(Img) {
     block_image_object=Img;
     
@@ -37,4 +37,82 @@ function player_update() {
     canvas.add(block_image_object);
         });
     
+    }
+
+
+
+    window.addEventListener("keydown", my_keydown);
+
+    function my_keydown(e) {
+    keyPressed=e.keyCode;
+    console.log(keyPressed);
+    if(e.shiftKey == true && keyPressed == '80') {
+    console.log("p and shift pressed together");
+    block_image_width = block_image_width + 10;
+    block_image_height = block_image_height + 10;
+    document.getElementById("current_width").innerHTML = block_image_width;
+    document.getElementById("current_height").innerHTML = block_image_height;
+    }
+
+    if(e.shiftKey && keyPressed == '77') {
+        console.log("m and shift pressed together");
+        block_image_width = block_image_width -  10;
+        block_image_height = block_image_height - 10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+
+
+    if(keyPressed == '30') {
+   up();
+   console.log("up");
+    }
+    if(keyPressed == '40') {
+    down();
+    console.log("down");
+    }
+    if(keyPressed == '37') {
+    left();
+    console.log("left");
+    }
+    if(keyPressed == '39') {
+    right();
+    console.log("right");
+    }
+    if(keyPressed == '87') {
+    new_image('wall.jpg');
+    console.log("w");
+    }
+    if(keyPressed == '71') {
+    new_image('ground.png');
+    console.log("g");
+    }
+    if(keyPressed == '76') {
+    new_image('light_green.png');
+    console.log("l");
+    }
+    if(keyPressed == '84') {
+    new_image('trunk.jpg');
+    console.log("t");
+    }
+    if(keyPressed == '82') {
+        new_image('roof.jpg');
+        console.log("r");
+    }
+    if(keyPressed == '89') {
+        new_image('yellow_wall.png');
+        console.log("y");
+    }
+    if(keyPressed == '68') {
+        new_image('dark_green.png');
+        console.log("d");
+    }
+    if(keyPressed == '85') {
+        new_image('different.png');
+        console.log("u");
+    }
+    if(keyPressed == '67') {
+        new_image('cloud.jpg');
+        console.log("c");
+    }
     }
