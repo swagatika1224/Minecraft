70184e5d5e22c054218ca72e0fe621bf4a50c244
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-06-18 18:43:57 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function new_image(get_image) {
     }
 
 
-    if(keyPressed == '30') {
+    if(keyPressed == '38') {
    up();
    console.log("up");
     }
@@ -116,3 +116,46 @@ function new_image(get_image) {
         console.log("c");
     }
     }
+
+
+
+function up() {
+if(player_y >=0) {
+player_y = player_y - block_image_height;
+console.log("block image height = " + block_image_height);
+console.log("When Up arrow key is pressed, X = " + player_x + " , Y = "+player_y);
+canvas.remove(player_object);
+player_update();
+    }
+}
+
+
+function down() {
+if(player_y <=500) {
+player_y = player_y + block_image_height;
+console.log("block image height = " + block_image_height);
+console.log("When Down arrow key is pressed, X = " + player_x + " , Y = "+player_y);
+canvas.remove(player_object);
+player_update();
+    }
+}
+
+
+function left() {
+if(player_x >0) {
+player_x = player_x - block_image_width;
+console.log("block image width = " + block_image_width);
+console.log("When Left arrow key is pressed, X = " + player_x + " , Y = "+player_y);
+canvas.remove(player_object);
+player_update();   
+    }
+}
+
+
+function right() {
+if(player_x <=850) {
+player_x = player_x + block_image_width;
+console.log("block image width = " + block_image_width);
+console.log("When Right arrow key is pressed, X = " + player_x + ", Y = "+player_y);
+    }
+}
